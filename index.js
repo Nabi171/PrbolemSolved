fc2fa0eda5a20895445ace84df59b3ad963258c2
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-07 20:45:54 +0600

    probelm1 added

---
+++ index.js
@@ -0,0 +1,20 @@
+// problem1
+
+function mindGame(num) {
+    if (num == 5) {
+        let result = (((num * 3) + 10) / 2) - 5;
+        return result;
+    }
+    else if (num == 50) {
+        let result = (((num * 3) + 10) / 2) - 5;
+        return result;
+    }
+    else if (num == 33) {
+        let result = (((num * 3) + 10) / 2) - 5;
+        return result;
+    }
+
+}
+
+let answer = mindGame(33);
+// console.log(answer);

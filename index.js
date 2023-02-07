509997934709f68cf576502f3ebb3d7c48ef4c53
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-07 22:49:34 +0600

    changes in adding

--- index.js
+++ index.js
@@ -57,4 +57,51 @@ function isLGSeven(num) {
 // let answertwo = isLGSeven(6);
 // let answertwo = isLGSeven(-15);
 let answertwo = isLGSeven(15);
-console.log(answertwo);
+// console.log(answertwo);
+
+
+// problem4
+function findingBadData(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        if (number < 0) {
+            console.log(number);
+        }
+        else if (number > 0) {
+            let result = 0;
+            console.log(result);
+        }
+    }
+
+}
+
+// let numbers = [1, 2, 5];
+let numbers = [2, -5, -7, -13];
+// let numbers = [2, -5, -7, -13];
+let answer4 = findingBadData(numbers);
+// console.log(answer4);
+
+
+
+
+
+
+
+// problem5 
+function gemsToDiamond(nums1, nums2, nums3) {
+    let firstBondhuPerjamesPower = 21;
+    let secondBondhuPerjamesPower = 32;
+    let thirdBondhuPerjamesPower = 43;
+
+    let toatalDiamond = firstBondhuPerjamesPower * nums1 + secondBondhuPerjamesPower * nums2 + thirdBondhuPerjamesPower * nums3;
+    // return toatalDiamond;
+    if (toatalDiamond > (1000 * 2)) {
+        let remaingingDiamong = toatalDiamond - 2000;
+        return remaingingDiamong;
+    }
+}
+
+// let answer5 = gemsToDiamond(1, 1, 1);
+// let answer5 = gemsToDiamond(20, 200, 50);
+let answer5 = gemsToDiamond(100, 5, 1);
+// console.log(answer5);

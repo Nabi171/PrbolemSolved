026b6a7a57d10bb93fc7110915ccdc44e58f426b
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-07 20:55:52 +0600

    chamges in p2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // problem1
-
 function mindGame(num) {
     if (num == 5) {
         let result = (((num * 3) + 10) / 2) - 5;
@@ -13,8 +12,26 @@ function mindGame(num) {
         let result = (((num * 3) + 10) / 2) - 5;
         return result;
     }
-
 }
-
 let answer = mindGame(33);
 // console.log(answer);
+
+
+
+
+// problem2
+function evenOdd(characters) {
+    let charactersNumber = characters.length;
+    if (charactersNumber % 2 == 1) {
+        console.log('This character is odd');
+    }
+    else if (charactersNumber % 2 == 0) {
+        console.log('This character is even');
+    }
+}
+// let characters = "phero";
+// let characters = "chatgpt";
+let characters = "Batch7";
+// evenOdd(characters);
+
+
